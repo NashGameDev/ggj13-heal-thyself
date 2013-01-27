4288fc237c22010ac5af49b6957ebf13443ec9f2
catfriedrice <catfriedrice@gmail.com>
2013-01-26 20:38:46 -0800

    Create commit.java

---
+++ commit.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package healthyself;
+
+/**
+ *
+ * @author catfriedrice
+ */
+public interface Common {
+    public static final int GAME_WIDTH = 600;
+    public static final int GAME_HEIGHT = 450;
+    public static final int BOTTOM_VEIN = 350;
+    public static final int TOP_VEIN = 10;
+    public static final int PLAYER_WIDTH = 190;
+    public static final int PLAYER_HEIGHT = 190;
+    public static final int UP_MOVE = 5;
+    public static final int DOWN_MOVE = 5;
+    public static final int LEFT_MOVE = 5;
+    public static final int RIGHT_MOVE = 5;
+    public static final int LEFT_BORDER = 10;
+    public static final int RIGHT_BORDER = 550;
+    public static final int ENEMY_TYPES = 1;
+    public static final int ENEMY_MAX_WIDTH = 190;
+    public static final int ENEMY_MAX_HEIGHT = 190;
+    public static final int INIT_HP = 100;
+    public static final int INIT_LIVES = 3;
+    public static final int INIT_DELAY = 30;
+    public static final int SHOT_SPEED = 10;
+    public static final int ENEMY_X_START = 600;       
+}
+
